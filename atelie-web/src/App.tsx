c60a8c1e5c89ffddce604cd6c9ff896c4619ca85
Jottinha <joaobrito.cassiano@gmail.com>
2026-01-13 12:12:27 -0300

    chore: configurar Tailwind corretamente no Vite + React TS

---
+++ atelie-web/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="p-6 text-2xl font-bold text-blue-600">
+      Tailwind funcionando 🚀
+    </div>
+  );
+}
